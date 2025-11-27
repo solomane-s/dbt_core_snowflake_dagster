96aeff7bedc9c81ba92b57d6563d7ec8faffd1ca
solomane-s <solomanesangare10@gmail.com>
2025-11-27 10:19:30 +0100

    change profile, add orchestration

--- orchestration/setup.py
+++ orchestration/setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dagster_config",
+    name="orchestration",
     version="0.0.1",
     packages=find_packages(),
     package_data={
-        "dagster_config": [
+        "orchestration": [
             "dbt-project/**/*",
         ],
     },
